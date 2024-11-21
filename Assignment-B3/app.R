36d47acbdfb53ab8479e6a63a73e09063b670717
Mia-UBC <miasi@student.ubc.ca>
2024-11-21 11:11:19 -0800

    Finally got the table to filter by multiple factors

--- Assignment-B3/app.R
+++ Assignment-B3/app.R
@@ -1,66 +1,19 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
 
-# Example code below, consult and delete
-
-# # Define UI for application that draws a histogram
-# ui <- fluidPage(
-# 
-#     # Application title
-#     titlePanel("Old Faithful Geyser Data"),
-# 
-#     # Sidebar with a slider input for number of bins 
-#     sidebarLayout(
-#         sidebarPanel(
-#             sliderInput("bins",
-#                         "Number of bins:",
-#                         min = 1,
-#                         max = 50,
-#                         value = 30)
-#         ),
-# 
-#         # Show a plot of the generated distribution
-#         mainPanel(
-#            plotOutput("distPlot")
-#         )
-#     )
-# )
-# 
-# # Define server logic required to draw a histogram
-# server <- function(input, output) {
-# 
-#     output$distPlot <- renderPlot({
-#         # generate bins based on input$bins from ui.R
-#         x    <- faithful[, 2]
-#         bins <- seq(min(x), max(x), length.out = input$bins + 1)
-# 
-#         # draw the histogram with the specified number of bins
-#         hist(x, breaks = bins, col = 'darkgray', border = 'white',
-#              xlab = 'Waiting time to next eruption (in mins)',
-#              main = 'Histogram of waiting times')
-#     })
-# }
-# 
-# # Run the application 
-# shinyApp(ui = ui, server = server)
-
+# Welcome to my shiny app
+## This code builds a shiny app which will allow the user to filter the palmer penguins dataset and download the resulting table.
 
 library(shiny)
 
 library(palmerpenguins)
+
+library(dplyr)
 
 # Code the ui below
 
 ui <- fluidPage(
   
   # App Title
-  titlePanel("Palmer Penguins"),
+  titlePanel("Palmer Penguins!"),
   
   # Create a sidebar and main panel
   sidebarLayout(
@@ -69,20 +22,25 @@ ui <- fluidPage(
     sidebarPanel(
       
       # Select which species the table will display. Can select multiple species if desired. This allows for the user to only see data from the species, singular or plural, that they are interested in.
-      selectInput("speciesIN", "species", choices = levels(penguins$species)),
+      
+      selectInput("speciesIN", "species", choices = levels(penguins$species), selected = NULL, multiple = TRUE),
+      
       # Select which inland(s) the table will display. This allows for the user to only see data from the island(s) that they are interested in.
-      selectInput("islandInput", "Island", choices = c("Biscoe", "Dream", "Torgersen"), multiple = TRUE),
+      
+      selectInput("islandIN", "island", choices = levels(penguins$island), selected = NULL, multiple = TRUE),
       
       # Select which sex(es) the table will display. This allows for the user to only see data from the sex(es) that they are interested in.
-      checkboxGroupInput("sexInput", "Sex", choices = c("female", "male")),
       
-      # Download the produced table. This allows the user to download the table with the data filters they have applied as a .csv file, so that they can use it as they please.
-      downloadButton("downloadOutput", "Download Table", icon("Download"))
+      checkboxGroupInput("sexIN", "sex", choices = c("female", "male"), selected = NULL),
     ),
     
     # Code for features in the main panel. 
-    ## This produces a table from the palmer penguins dataset. As the DT package is used, the produced table has several filtering features, which allow the user to sort the data as they desire.
-    mainPanel(tableOutput("my_table"))
+    
+    mainPanel(
+      
+      # This produces a table from the *palmerpenguins* dataset. As the DT package is used, the resulting table will be compact, and come with some basic filtering and search abilities for the user.
+      
+      DT::dataTableOutput("my_table"))
   )
 )
 
@@ -90,18 +48,20 @@ ui <- fluidPage(
 
 server <- function(input, output) {
 
-  # Renders the table in the main panel
-  #output$pengs <- DT::renderDataTable(penguins)
+  # Renders the table in the main panel. As the DT package is used, this also implements several features for filtering and searching the data.
   
-  output$my_table <- renderTable({
-   subset(penguins, species == input$speciesIN)
-   })
+  filtered_penguins <- reactive({
+    penguins %>% 
+      filter(
+        (is.null(input$speciesIN) | species %in% input$speciesIN) & 
+        (is.null(input$islandIN) | island %in% input$islandIN) &
+        (is.null(input$sexIN) | sex %in% input$sexIN)
+      )
+  })
   
-  # Allows the download button to function. This will write the data as a .csv file.
-  output$downloadOutput <- downloadHandler("Palmer Penguins Table.csv", content = function(file) {
-    write.csv(PalmerPenguinTable, file)
-  }
-  )
+  output$my_table <- DT::renderDataTable({
+    filtered_penguins()
+  })
 }
 
 # The following line MUST be the last line in the file; delete anything after it before running the app
